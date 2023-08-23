aa30140479cb05791b3846997918a2ea06209ed0
EdenSwack <93931254+EdenSwack@users.noreply.github.com>
2023-08-24 02:29:24 +0300

    Update invert_func.py

--- invert_func.py
+++ invert_func.py
@@ -20,15 +20,30 @@ stwords = set(stopwords.words('english'))
 lemmatizer = WordNetLemmatizer()
 
 
+    """
+A class that implements an inverted index for text documents.
+    """
 class InvIndex:
 
+
+     """
+Initializes the inverted index by creating the index map.
+docs: List of text documents.
+        """
     def __init__(self,docs):
         self.inverted_index_map=self.index_words(docs)
 
-
+"""
+Creates an inverted index from a list of text documents.
+        """
     def index_words(self, docs):
         inverted_index = defaultdict(set)
 
+"""
+        return: Inverted index as a defaultdict of sets.
+"""
+
+        
         for docId, f in enumerate(docs):
             docId+=1
             f=self.remove_punctuation(f)
@@ -44,6 +59,12 @@ class InvIndex:
         return (re.sub(r'[^\w\s]', "", file))
 
     def run_query(self,query):
+
+ """
+Executes a query on the inverted index and returns matching documents.
+    :return: List of document IDs matching the query.
+"""
+        
         #processing:
         matched_document_nums=set()
         for word in word_tokenize(query):
@@ -57,6 +78,12 @@ class InvIndex:
 
 
     def search(self,word, matched_docs):
+ """
+Searches the inverted index for documents containing a word.
+    :param word: Word to search for.
+    :param matched_docs: Set of matched document IDs.
+    :return: Updated set of matched document IDs.
+"""
         matches = self.inverted_index_map.get(word)
         if matches:
             if (len(matched_docs) == 0):
@@ -66,6 +93,11 @@ class InvIndex:
         return matched_docs
 
     def process(self,word):
+
+"""
+Processes a word by converting to lowercase and lemmatizing if not a stopword.
+    :return: Processed word.
+"""
         word_lower = word.lower()
         if word_lower not in stwords:
             word_lemm = lemmatizer.lemmatize(word_lower)
